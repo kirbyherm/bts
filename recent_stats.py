1beff10d6ca62decd160b1560c5be06664e21954
Kirby Hermansen <herman67@msu.edu>
2018-02-06 19:29:10 -0500

    temp

--- recent_stats.py
+++ recent_stats.py
@@ -1,28 +1,61 @@
 from lxml import html
+#from mechanize import Browser
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+from tqdm import tqdm
 
-
-def get_recent(soup):
+def get_recent(soup,filename,playerID):
   table = soup.findAll('table')
-  recent_table = table[2]
+  try:
+    recent_table = table[2]
+    for row in recent_table.findAll('tr')[1:]:
+      col = row.findAll('td')
+      rowname = col[0].string
+      AB = int(col[1].string)
+      R = int(col[2].string)
+      H = int(col[3].string)
+      doub = int(col[4].string)
+      trip = int(col[5].string)
+      HR = int(col[6].string)
+      RBI = int(col[7].string)
+      BB = int(col[8].string)
+      AVG = float(col[9].string)
+    
+      total = (rowname,str(AB),str(R),str(H),str(doub),str(trip),str(HR),str(RBI),str(BB),str(AVG))
+    file1 = open(filename,'a')
+    file1.write(str(playerID))
+    file1.write('\n')
+    file1.close()
+  except:
+    return 0
+
+  return 1
   header=0 
   i = 0
 # for col in recent_table.findAll('th')[1:]:
 #   header[i] = col.string
 #   i = i+1  
   
-  for row in recent_table.findAll('tr')[1:]:
-    col = row.findAll('td')
-    rowname = col[0].string
-    AB = int(col[1].string)
-    R = int(col[2].string)
-    H = int(col[3].string)
-    doub = int(col[4].string)
-    trip = int(col[5].string)
-    HR = int(col[6].string)
-    RBI = int(col[7].string)
-    BB = int(col[8].string)
-    AVG = float(col[9].string)
-    
-    total = (rowname,str(AB),str(R),str(H),str(doub),str(trip),str(HR),str(RBI),str(BB),str(AVG))
-    print(str.join('|',total))
 
+
+def loop_recent():
+  filename = ('/home/khermans/Documents/bts/test.txt')
+  total = 0
+#  bar = progressbar.ProgressBar(maxval=29999,\
+#    widgets=[progressbar.Bar('#','[',']'),' ',progressbar.Percentage()])
+#  bar.start()
+  for x in tqdm(range(7000,8000)):
+#  for x in tqdm(range(1,3000)):
+    playerID = x
+    playerpage = 'http://www.espn.com/mlb/player/_/id/'+str(x)
+    page = requests.get(playerpage)
+    soup = BeautifulSoup(page.content,'html.parser')
+    y = get_recent(soup,filename,playerID)
+#    print(x,'|',y)
+    total = total + y
+#    bar.update(x)
+#    sleep(0.1)
+#  bar.finish()
+  print(total)
+loop_recent()
